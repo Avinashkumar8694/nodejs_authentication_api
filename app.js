70525fc5d6691e90782b7d8d1439d3af690b4093
Avinash Kumar Gupta <avinash.gupta@neutrinos.co>
2021-07-27 19:59:35 +0530

    added dotenv

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
 
+dotenv.config();
 
 app.listen(3000, () => console.log('3000 port server running'));
