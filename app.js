31d7b3c117e8e65d79954a8930100e396124429b
Avinash Kumar Gupta <avinash.gupta@neutrinos.co>
2021-07-27 20:01:55 +0530

    added mongo db connection

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-
+const mongoose = require("mongoose");
 dotenv.config();
 
-app.listen(3000, () => console.log('3000 port server running'));
+// Mongodb Connection
+// add DB_CONNECT_CONNECTION_STRING and TOKEN_SECRET in .env file
+mongoose.connect(
+  process.env.DB_CONNECT_CONNECTION_STRING,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("db connected");
+  }
+);
+
+app.listen(3000, () => console.log("3000 port server running"));
